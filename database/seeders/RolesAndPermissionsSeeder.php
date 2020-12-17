3215dccd1da10e934f3447f9efb4b71208e24887
Giulio Troccoli-Allard <giulio@troccoli.it>
2020-12-17 18:23:52 +0000

    Replaced Laratrust package with Laravel Permission from Spatie

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace database\seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Permission::create(['name' => 'register users']);
+
+        Role::create(['name' => 'Super Admin']);
+        Role::create(['name' => 'Editor']);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->app->make(\Spatie\Permission\PermissionRegistrar::class)->registerPermissions();
+    }
+
+
 }
