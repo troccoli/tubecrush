827331eed9fe0e54a7bf7f5fc8205de3d4cc07f2
Giulio Troccoli-Allard <giulio@troccoli.it>
2021-01-15 16:23:23 +0000

    Fixed registration of DuskServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,9 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        if (!$this->app->environment('production')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
     }
 
     public function boot(): void
--- app/Providers/DuskServiceProvider.php
+++ app/Providers/DuskServiceProvider.php
@@ -10,17 +10,20 @@ class DuskServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Browser::macro('assertCountInElement', function (int $expectedCount, string $selector, string $message = null): Browser {
-            /** @var Browser $this */
+        if (!$this->app->environment('production')) {
+            Browser::macro('assertCountInElement',
+                function (int $expectedCount, string $selector, string $message = null): Browser {
+                    /** @var Browser $this */
 
-            $elements = $this->resolver->all($selector);
-            PHPUnit::assertCount(
-                $expectedCount,
-                $elements,
-                $message ?? "Failed asserting that actual size ".count($elements)." matches expected size $expectedCount for elements [{$selector}]."
-            );
+                    $elements = $this->resolver->all($selector);
+                    PHPUnit::assertCount(
+                        $expectedCount,
+                        $elements,
+                        $message ?? "Failed asserting that actual size ".count($elements)." matches expected size $expectedCount for elements [{$selector}]."
+                    );
 
-            return $this;
-        });
+                    return $this;
+                });
+        }
     }
 }
