df5427c7899b512d14eddc781f69b9fa1069f5e7
Giulio Troccoli-Allard <giulio@troccoli.it>
2021-02-20 15:25:52 +0000

    Fixed responsive design for Upload a photo button

--- resources/views/livewire/posts/create-post.blade.php
+++ resources/views/livewire/posts/create-post.blade.php
@@ -25,8 +25,9 @@
         <!-- Photo -->
         <div class="mb-6">
             <x-jet-label for="photo" dusk="upload-photo-button"
-                         class="inline-flex items-center p-2 pr-4 w-auto border border-transparent rounded bg-indigo-600 text-indigo-100 hover:bg-indigo-700 cursor-pointer transition duration-300">
+                         class="relative md:static flex md:inline-flex justify-center p-2 pr-4 w-full md:w-auto border border-transparent rounded bg-indigo-600 text-indigo-100 hover:bg-indigo-700 cursor-pointer transition duration-300">
                 <x-jet-input id="photo" type="file" class="hidden" wire:model="photo"/>
+                <span class="absolute md:static left-0 inset-y-0 flex items-center pl-3 md:pl-0">
                 <svg wire:loading.remove wire:target="photo" class="w-5 h-5 mr-1" xmlns="http://www.w3.org/2000/svg"
                      viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd"
@@ -39,7 +40,7 @@
                     <path class="opacity-75" fill="currentColor"
                           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
                     </path>
-                </svg>
+                </svg></span>
                 Upload a photo
             </x-jet-label>
             <x-jet-input-error for="photo" class="mt-2" dusk="photo-error"/>
