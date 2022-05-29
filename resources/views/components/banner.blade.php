b02868201a05f0cf8badbdfa36a99e0b6ec03fb6
Giulio Troccoli-Allard <giulio@troccoli.it>
2022-05-29 13:28:52 +0100

    Added first Cypress test

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-wrap justify-center">
+<div class="flex flex-wrap justify-center" id="banner">
     <img src="https://tubecrush.net/wp-content/uploads/2018/08/cropped-Logo-TC-BlkBk.jpg" alt="Site Banner"
          class="shadow rounded max-w-full h-auto align-middle border-none"/>
 </div>
