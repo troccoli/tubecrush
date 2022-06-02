038f673f4abcec867ccd1ce820bc1cd782122490
Giulio Troccoli-Allard <giulio@troccoli.it>
2022-06-02 11:53:18 +0100

    Fixed version of Cypress in GitHub Action CI

---
+++ cypress.config.js
@@ -0,0 +1,40 @@
+const {defineConfig} = require("cypress");
+const fs = require("fs");
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: 'http://localhost',
+        chromeWebSecurity: false,
+        defaultCommandTimeout: 5000,
+        retries: {
+            runMode: 2,
+            openMode: 2
+        },
+        setupNodeEvents(on, config) {
+            on('task', {
+                activateCypressEnvFile() {
+                    if (fs.existsSync('.env.cypress')) {
+                        fs.renameSync('.env', '.env.backup');
+                        fs.renameSync('.env.cypress', '.env');
+                    }
+
+                    return null;
+                },
+
+                activateLocalEnvFile() {
+                    if (fs.existsSync('.env.backup')) {
+                        fs.renameSync('.env', '.env.cypress');
+                        fs.renameSync('.env.backup', '.env');
+                    }
+
+                    return null;
+                },
+
+                log(message) {
+                    console.log(message)
+                    return null
+                },
+            })
+        },
+    },
+});
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,5 @@
 // ***********************************************************
-// This example support/index.js is processed and
+// This example support/e2e.js is processed and
 // loaded automatically before your test files.
 //
 // This is a great place to put global configuration and
@@ -13,11 +13,14 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-/// <reference types="./" />
-
+// Import commands.js using ES2015 syntax:
+import './commands'
 import './laravel-commands';
 import './laravel-routes';
 import './assertions';
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
 
 before(() => {
     cy.task('activateCypressEnvFile', {}, {log: false});
